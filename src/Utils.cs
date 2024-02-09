2062e03cf16f3180c60e6d8fb59105fd7d444c2f
Max <max@maximilian.co.nz>
2024-02-09 19:05:14 +1300

    ffmpeg commands work, and are multi-threaded

--- src/Utils.cs
+++ src/Utils.cs
@@ -4,21 +4,31 @@ class Utils
 {
 	// Run an FFMPEG command
 	// TODO: Maybe make a copy of the file before modifying
+	// TODO: Make a new thread to run the commands in
 	public static void RunFfmpegCommand(string args)
 	{
 		// TODO: Put this somewhere else
-		string outputPath = "./assets/test.mp4";
+		string outputPath = "./assets/output.mp4";
 
 		// Create the command to run
 		ProcessStartInfo command = new ProcessStartInfo
 		{
-			FileName = VideoManager.FfmpegPath,
+			FileName = VideoManager.FfmpegPath + "/ffmpeg.exe",
 			Arguments = $"-i {VideoManager.VideoPath} {args} {outputPath}",
 			UseShellExecute = false,
 			RedirectStandardOutput = true,
 			RedirectStandardError = true
 		};
 
+		// Make a new thread to run the command in
+		Thread commandThread = new Thread(() => RunCommand(command));
+		commandThread.Start();
+		
+		Console.WriteLine("Ran command");
+	}
+
+	private static void RunCommand(ProcessStartInfo command)
+	{
 		// Run the command
 		Process? ffmpeg = Process.Start(command);
 
@@ -26,6 +36,9 @@ class Utils
 		// TODO: Do something with these
 		string output = ffmpeg.StandardOutput.ReadToEnd();
 		string error = ffmpeg.StandardError.ReadToEnd();
+
+		Console.WriteLine(output);
+		Console.WriteLine(error);
 
 		// Wait for the command to finish running
 		// this could take a while because working with
