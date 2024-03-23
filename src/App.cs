8b79c61c57ae3d501d26219899696f4da4344162
MaximilianMcC <max@maximilian.co.nz>
2024-03-23 14:13:50 +1300

    restarting (new found knowledge)

--- src/App.cs
+++ src/App.cs
@@ -14,9 +14,9 @@ class App
 
 		// Raylib stuff
 		// TODO: Switch library (this is NOT a game!!)
-		Raylib.InitWindow(700, 500, "Clipper r1");
 		Raylib.SetConfigFlags(ConfigFlags.ResizableWindow);
 		Raylib.SetConfigFlags(ConfigFlags.AlwaysRunWindow);
+		Raylib.InitWindow(700, 500, "Clipper r2");
 		Raylib.SetTargetFPS(60);
 
 
@@ -32,51 +32,18 @@ class App
 
 	private static void Start()
 	{
-		// TODO: Count how long it takes to load video
-		VideoManager.LoadVideo();
-		Console.WriteLine("loaded video (sweet as!!)");
 
-		
-
-		//! temp
-		testFrame = VideoManager.LoadFrame(0);
 	}
 
 	private static void Update()
 	{
-		// Toggle debug mode
-		if (Raylib.IsKeyPressed(KeyboardKey.F3) || Raylib.IsKeyPressed(KeyboardKey.Grave)) debugMode = !debugMode;
 
-
-		// Update the video
-		VideoManager.UpdateVideo();
 	}
 
 	private static void Render()
 	{
 		Raylib.BeginDrawing();
 		Raylib.ClearBackground(Color.Magenta);
-
-				// Draw the frmae upside down because opengl is stupid
-		// they draw everything upside down
-		Rectangle source = new Rectangle(0, 0, testFrame.Width, -testFrame.Height);
-		Rectangle destination = new Rectangle(0, 0, Raylib.GetScreenWidth(), Raylib.GetScreenHeight());
-		Raylib.DrawTexturePro(testFrame, source, destination, Vector2.Zero, 0f, Color.White);
-
-		
-		
-		// Render the video
-		VideoManager.RenderVideo();
-
-
-
-		// Debug stuff
-		if (debugMode)
-		{
-			Vector2 size = Raylib.MeasureTextEx(Raylib.GetFontDefault(), VideoManager.Diagnostics(), 35f, (25f / 10f));
-			Raylib.DrawRectangle(0, 0, (int)size.Y, (int)size.X / 2, new Color(0, 0, 0, 128));
-			Raylib.DrawText(VideoManager.Diagnostics(), 10, 10, 35, Color.White);
-		}
 
 
 
@@ -85,8 +52,6 @@ class App
 
 	private static void CleanUp()
 	{
-		// Unload the video
-		VideoManager.UnloadVideo();
 
 		// Close the window
 		//! Make sure to do this last.
--- src/Program.cs
+++ src/Program.cs
@@ -2,20 +2,6 @@ class Program
 {
 	public static void Main(string[] args)
 	{
-		// Check for if they provided a video location
-		// TODO: Check for if there is actually a video in the directory
-		if (args.Length <= 0)
-		{
-			Console.WriteLine("You are missing the path to the video you wish to open.");
-			return;
-		}
-
-		// TODO: Check for if they actually have FFMPEG installed
-		
-
-		// Set the video path
-		// then run the app
-		VideoManager.Path = args[0].Trim();
 		App.Run();
 	}
 }
